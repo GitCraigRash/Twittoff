410c5fc8c45ebf818d7177c1bfd7761eccbc7923
Craig Rash Jr <90971454+GitCraigRash@users.noreply.github.com>
2023-06-14 23:46:30 -0400

    Updated to display latest tweet of specified twitter account

--- twitoff/models.py
+++ twitoff/models.py
@@ -1,4 +1,3 @@
-
 from flask_sqlalchemy import SQLAlchemy
 
 # create a DB object form SQLAchemy class
@@ -13,7 +12,7 @@ class User(DB.Model):
     username = DB.Column(DB.String, nullable=False)
     # backref is as if we have added a tweets list to the user class.
     # tweets = []
-    
+    newest_tweet_id = DB.Column(DB.BigInteger)
     def __repr__(self):
         return f"User: {self.username}"
 
@@ -23,12 +22,15 @@ class Tweet(DB.Model):
     id = DB.Column(DB.BigInteger, primary_key=True, nullable=False)
     # text column
     text = DB.Column(DB.Unicode(300))
+    #vectorizatoin column
+    vect = DB.Column(DB.PickleType, nullable = False)
     # user_id column
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'),nullable=False)
+
+    since_id=DB.Column(DB.BigInteger)
     # user column create a two-way link 
     # between a user obejct and a tweeted obejct.
     user = DB.relationship('User', backref=DB.backref('tweets',lazy=True))
 
     def __repr__(self):
         return f"Tweet: {self.text}"
-

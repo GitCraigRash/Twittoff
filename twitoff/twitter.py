60637d15a319aefcd319d19f80091a6f5b4749e7
Craig Rash Jr <90971454+GitCraigRash@users.noreply.github.com>
2023-06-14 23:44:22 -0400

    added forms for users to imput twitter accounts, forms to provide hypothetical tweet, button to compare who most likely said it.

--- twitoff/twitter.py
+++ twitoff/twitter.py
@@ -1,9 +1,9 @@
 '''Handles connection to NotTiwtter API using NotTweepy'''
 
 from os import getenv
-import not_tweepy.user as tweepy
 import spacy
-from twitoff.models import DB, Tweet, User
+import not_tweepy as tweepy
+from .models import DB, Tweet, User
 
 # Get API Key from environment vars.
 key = getenv('TWITTER_API_KEY')
@@ -16,28 +16,47 @@ TWITTER = tweepy.API(TWITTER_AUTH)
 
 def add_or_update_user(username):
     '''Takes username and pulls user from Twitter API'''
-    twitter_user = TWITTER.get_user(screen_name=username)
-    # Is there a user in the database that already has this id?
-    # If not, then create a User in the database with this id.
-    db_user = (User.query.get(twitter_user.id)) or User(id=twitter_user.id, username=username)
+    try:
+        twitter_user = TWITTER.get_user(screen_name=username)
+        # Is there a user in the database that already has this id?
+        # If not, then create a User in the database with this id.
+        #print(twitter_user.user_data)
+        db_user = (User.query.get(twitter_user.screen_name)) or User(id=twitter_user.screen_name, username=username)
 
-    # add the user to the database.
-    DB.session.add(db_user)
+        # add the user to the database.
+        DB.session.add(db_user)
 
-    # get the user's tweets
-    tweets = twitter_user.timeline(count=200, 
-                                   exclude_replies=True, 
-                                   include_rts=False, 
-                                   tweet_mode='extended')
+        # get the user's tweets
+        
+        tweets = twitter_user.timeline(count=200, 
+                                    exclude_replies=True, 
+                                    include_rts=False, 
+                                    tweet_mode='extended')
+        
+        
+        
+        # add each tweet to the database
+        for tweet in tweets:
+            tweet_vector = vectorize_tweet(tweet.full_text)
+            db_user.newest_tweet_id = tweets[0].id
+            db_tweet = Tweet(id=tweet.id,
+                             text=tweet.full_text[:300],
+                             user_id=db_user.id,
+                             vect = tweet_vector,
+                             since_id=db_user.newest_tweet_id)
+        if tweets:
+            db_user.newest_tweet_id = tweets[0].id
+            DB.session.add(db_tweet)
+        
 
-    # add each tweet to the database
-    for tweet in tweets:
-        db_tweet = Tweet(id=tweet.id,
-                         text=tweet.full_text[:300],
-                         user_id=db_user.id)
-        db_user.tweets.append(db_tweet)
-        DB.session.add(db_tweet)
+    except Exception as e:
+        print(f"Error Processing {username}: {e}")
+        raise e
+    else:
+        # Save the changes to the DB
+        DB.session.commit()
 
-    # Save the changes to the DB
-    DB.session.commit()
-    
+nlp = spacy.load('my_model/')
+#We have the same too we used in the flask shell
+def vectorize_tweet(tweet_text):
+    return nlp(tweet_text).vector
